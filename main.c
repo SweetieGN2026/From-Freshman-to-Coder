866557a52dfb1ee1d1fedfcfb2d0af5119e3cb79
SweetieGN2026 <2013004001@qq.com>
2026-02-05 17:28:55 +0800

    大一.2

--- main.c
+++ main.c
@@ -3,20 +3,19 @@
 #include <time.h>
 
 int main() {
-    srand((unsigned int)time(NULL));  // 随机数种子，保证每次数字不同
-    int number = rand() % 100 + 1;       // 生成1-100随机答案
-    int guess, n = 0;                 // guess：玩家输入数  n：猜测次数
-
-    while (1) {//始终运行  除非猜中
-        printf("猜1-100的数：");
-        scanf("%d", &guess);
-        n++;
-        if (guess > number) printf("大了\n");
-        else if (guess < number) printf("小了\n");
-        else {
-            printf("猜对了！共猜%d次\n", n);
-            break;
-        }
+    char *guess[] = {"", "石头", "剪刀", "布"};
+    int player, computer;
+    srand((unsigned int)time(NULL)); // 随机数种子，保证电脑出拳不重复
+    computer = rand() % 3 + 1;       // 简单的数学逻辑 如果有四种可能就除以4
+    printf("石头剪刀布：1=石头 2=剪刀 3=布\n请出拳：");
+    scanf("%d", &player);
+    printf("你出：%s  电脑出：%s\n", guess[player], guess[computer]);
+    if (player == computer) {
+        printf("平局！\n");
+    } else if ((player==1&&computer==2) || (player==2&&computer==3) || (player==3&&computer==1)) {
+        printf("你赢了！\n");
+    } else {
+        printf("你输了！\n");
     }
     return 0;
 }
